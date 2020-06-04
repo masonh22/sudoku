e227261f6050c5cc0c3f9898377a145e023c2dbf
Mason Hall <mhall22990@gmail.com>
2020-06-04 19:23:46 -0400

    Updates

---
+++ precache-manifest.c960d68fa425dc05cfc52663c6bc6532.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5e56455eb661aa3416dedd872fce2fe",
+    "url": "/sudoku/index.html"
+  },
+  {
+    "revision": "19edab3de5224b69508d",
+    "url": "/sudoku/static/css/main.3957ea45.chunk.css"
+  },
+  {
+    "revision": "7486ddc2cd20da671f51",
+    "url": "/sudoku/static/js/2.f7796eda.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sudoku/static/js/2.f7796eda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19edab3de5224b69508d",
+    "url": "/sudoku/static/js/main.fd673156.chunk.js"
+  },
+  {
+    "revision": "5e467a82d5680057536a",
+    "url": "/sudoku/static/js/runtime-main.e006c59b.js"
+  }
+]);
